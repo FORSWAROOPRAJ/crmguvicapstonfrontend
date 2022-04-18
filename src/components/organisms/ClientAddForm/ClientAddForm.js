e6c49cbdb55fdb46394f7f7f6b51150c1b733e52
SWAROOP R <60713891+FORSWAROOPRAJ@users.noreply.github.com>
2022-04-19 04:29:36 +0530

    Update ClientAddForm.js

--- src/components/organisms/ClientAddForm/ClientAddForm.js
+++ src/components/organisms/ClientAddForm/ClientAddForm.js
@@ -54,7 +54,7 @@ export const ClientAddForm = () => {
         label="Category"
         name="category"
         id="category"
-        options={['WebServices','AndroidApp','iOSapp']} />
+        options={[`WebServices`·`AndroidApp`·`iOSapp`]} />
       {errors.category ? (
         <FormInputError>Please fill category field</FormInputError>
       ) : null}
@@ -63,7 +63,7 @@ export const ClientAddForm = () => {
         label="Status"
         name="status"
         id="status"
-        options={['lead', 'contract', 'completed']}
+        options={[`lead`·`contract`·`completed`]}
       />
       {errors.status ? (
         <FormInputError>Please set status</FormInputError>
